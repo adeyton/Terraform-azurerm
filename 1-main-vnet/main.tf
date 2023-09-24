bd37bfa72340946960caa26785e1157ed716aa45
Komal Deyton <komaldeyton@gmail.com>
2023-09-23 20:47:16 -0400

    feat: change the rg variable and value

--- 1-main-vnet/main.tf
+++ 1-main-vnet/main.tf
@@ -15,8 +15,9 @@ terraform {
 # VARIABLES
 #############################################################################
 
-variable "resource_group_name" {
+variable "rg_name" {
   type = string
+  default = "stupid"
 }
 
 variable "location" {
@@ -53,7 +54,7 @@ provider "azurerm" {
 #############################################################################
 
 resource "azurerm_resource_group" "vnet_main" {
-  name     = var.resource_group_name
+  name     = var.rg_name
   location = var.location
 }
 
@@ -61,7 +62,7 @@ module "vnet-main" {
   source              = "Azure/vnet/azurerm"
   version             = "~> 2.0"
   resource_group_name = azurerm_resource_group.vnet_main.name
-  vnet_name           = var.resource_group_name
+  vnet_name           = var.rg_name
   address_space       = [var.vnet_cidr_range]
   subnet_prefixes     = var.subnet_prefixes
   subnet_names        = var.subnet_names
